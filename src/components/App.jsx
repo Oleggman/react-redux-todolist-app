16f7fb3d094b6caf53afc29c6d6783dcdadff9e7
Олег <olegkovalets1112003@knu.ua>
2023-10-31 11:46:36 +0200

    Test commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import { Layout } from "./Layout/Layout";
+import { ActivityBar } from "./ActivityBar/ActivityBar";
 import { TaskForm } from "./TaskForm/TaskForm";
 import { TaskList } from "./TaskList/TaskList";
-import { ActivityBar } from "./ActivityBar/ActivityBar";
 
 export const App = () => {
   return (
     <Layout>
-      <TaskForm />
       <ActivityBar />
+      <TaskForm />
       <TaskList />
     </Layout>
   );
