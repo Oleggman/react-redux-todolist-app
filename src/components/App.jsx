f3b5286feda0103c5fef39a662b844173c0c1a53
Олег <olegkovalets1112003@knu.ua>
2023-10-31 11:40:14 +0200

    Fix build errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import { Layout } from "components/Layout/Layout";
-import { AppBar } from "components/AppBar/AppBar";
-import { TaskForm } from "components/TaskForm/TaskForm";
-import { TaskList } from "components/TaskList/TaskList";
+import { Layout } from "./Layout/Layout";
+import { TaskForm } from "./TaskForm/TaskForm";
+import { TaskList } from "./TaskList/TaskList";
+import { AppBar } from "./AppBar/AppBar";
 
 export const App = () => {
   return (
     <Layout>
-      <AppBar />
       <TaskForm />
+      <AppBar />
       <TaskList />
     </Layout>
   );
